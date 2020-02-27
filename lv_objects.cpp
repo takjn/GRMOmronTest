276755f7479d134ef71a4d7d8cd973a9c25ca75a
Jun Takeda <takeda.jun@gmail.com>
2020-02-28 01:26:47 +0900

    fix: clean up

--- lv_objects.cpp
+++ lv_objects.cpp
@@ -111,6 +111,15 @@ void lv_objects(void)
     lv_slider_set_range(slider, 0, 15);
     lv_slider_set_value(slider, 7, false);
 
+    static lv_style_t st;
+    lv_style_copy( &st, &lv_style_plain );
+    st.text.color = LV_COLOR_BLACK;
+    st.text.font = &lv_font_roboto_28;
+    lv_obj_t * title = lv_label_create(scr, NULL);
+    lv_label_set_text(title, "GR-MANGO OMRON sensor evaluation kit");
+    lv_obj_set_style(title, &st);
+    lv_obj_align(title, slider, LV_ALIGN_IN_TOP_LEFT, 0,100);
+
 
     /********************************************************************************
      * RIGHT
@@ -212,92 +221,8 @@ void lv_objects(void)
     lv_label_set_text(label5, "[2SMPB-02E] MEMS digital barometric pressure sensor [degC]");
     lv_obj_align(label5, chart4, LV_ALIGN_OUT_BOTTOM_LEFT, 10, 30);
 
-    // /***********************
-    //  * CREATE TWO BUTTONS
-    //  ***********************/
-    // /*Create a button*/
-    // lv_obj_t * btn1 = lv_btn_create(lv_disp_get_scr_act(NULL), NULL);         /*Create a button on the currently loaded screen*/
-    // lv_obj_set_event_cb(btn1, btn_event_cb);                                  /*Set function to be called when the button is released*/
-    // lv_obj_align(btn1, label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 20);               /*Align below the label*/
-
-    // /*Create a label on the button (the 'label' variable can be reused)*/
-    // label = lv_label_create(btn1, NULL);
-    // lv_label_set_text(label, "Button 1");
-
-    // /*Copy the previous button*/
-    // lv_obj_t * btn2 = lv_btn_create(scr, btn1);                 /*Second parameter is an object to copy*/
-    // lv_obj_align(btn2, btn1, LV_ALIGN_OUT_RIGHT_MID, 50, 0);    /*Align next to the prev. button.*/
-
-    // /*Create a label on the button*/
-    // label = lv_label_create(btn2, NULL);
-    // lv_label_set_text(label, "Button 2");
-
-    // /***********************
-    //  * ADD A DROP DOWN LIST
-    //  ************************/
-    // lv_obj_t * ddlist = lv_ddlist_create(scr, NULL);                     /*Create a drop down list*/
-    // lv_obj_align(ddlist, slider, LV_ALIGN_OUT_RIGHT_TOP, 50, 0);         /*Align next to the slider*/
-    // lv_obj_set_top(ddlist, true);                                        /*Enable to be on the top when clicked*/
-    // lv_ddlist_set_options(ddlist, "None\nLittle\nHalf\nA lot\nAll");     /*Set the options*/
-    // lv_obj_set_event_cb(ddlist, ddlist_event_cb);                        /*Set function to call on new option is chosen*/
-
-    // /****************
-    //  * CREATE A CHART
-    //  ****************/
-    // lv_obj_t * chart = lv_chart_create(scr, NULL);                         /*Create the chart*/
-    // lv_obj_set_size(chart, lv_obj_get_width(scr) / 2, lv_obj_get_width(scr) / 4);   /*Set the size*/
-    // lv_obj_align(chart, slider, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 50);                   /*Align below the slider*/
-    // lv_chart_set_series_width(chart, 3);                                            /*Set the line width*/
-
-    // /*Add a RED data series and set some points*/
-    // lv_chart_series_t * dl1 = lv_chart_add_series(chart, LV_COLOR_RED);
-    // lv_chart_set_next(chart, dl1, 10);
-    // lv_chart_set_next(chart, dl1, 25);
-    // lv_chart_set_next(chart, dl1, 45);
-    // lv_chart_set_next(chart, dl1, 80);
-
-    // /*Add a BLUE data series and set some points*/
-    // lv_chart_series_t * dl2 = lv_chart_add_series(chart, lv_color_make(0x40, 0x70, 0xC0));
-    // lv_chart_set_next(chart, dl2, 10);
-    // lv_chart_set_next(chart, dl2, 25);
-    // lv_chart_set_next(chart, dl2, 45);
-    // lv_chart_set_next(chart, dl2, 80);
-    // lv_chart_set_next(chart, dl2, 75);
-    // lv_chart_set_next(chart, dl2, 505);
-
 }
 
 /**********************
  *   STATIC FUNCTIONS
  **********************/
-
-/**
- * Called when a button is released
- * @param btn pointer to the released button
- * @param event the triggering event
- * @return LV_RES_OK because the object is not deleted in this function
- */
-static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
-{
-    if(event == LV_EVENT_RELEASED) {
-        /*Increase the button width*/
-        lv_coord_t width = lv_obj_get_width(btn);
-        lv_obj_set_width(btn, width + 20);
-    }
-}
-
-/**
- * Called when a new option is chosen in the drop down list
- * @param ddlist pointer to the drop down list
- * @param event the triggering event
- * @return LV_RES_OK because the object is not deleted in this function
- */
-static  void ddlist_event_cb(lv_obj_t * ddlist, lv_event_t event)
-{
-    if(event == LV_EVENT_VALUE_CHANGED) {
-        uint16_t opt = lv_ddlist_get_selected(ddlist);            /*Get the id of selected option*/
-
-        lv_slider_set_value(slider, (opt * 100) / 4, true);       /*Modify the slider value according to the selection*/
-    }
-
-}
